6b6311eeb11c176fc5e41d37e75505df9fdb386d
TrimacPanel <it@trimacpanel.com>
2019-12-03 15:35:47 -0800

    FXML GUI built
    Probably have several more things to add. But the tableview has been added. Along with a menu bar and a refresh button. No IDs have been added yet and no controller functions have been created.

--- src/model/Main.java
+++ src/model/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package model;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/mainscreen.fxml"));
+        primaryStage.setTitle("Production Scheduler");
+        primaryStage.setScene(new Scene(root, 1000, 700));
         primaryStage.show();
     }
 
